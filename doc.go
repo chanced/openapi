c419a62af7b315bd4ce8db03fdaca11900be43ea
chanced <chanceusc@gmail.com>
2021-11-30 16:23:56 -0500

    adds doc.go

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Package openapi is a set of Go types for
+// [OpenAPI Specification 3.1](https://spec.openapis.org/oas/v3.1.0). The primary purpose of the package
+// is to assist in generation of OpenAPI documentation or to offer building blocks for code-generation.
+//
+// Validation of specifications is performed with JSON Schema.
+package openapi
